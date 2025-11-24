750f27eaa3b53966ee4407005501654c0f855486
Abdalla-El-gohary <rabyalalla@gmail.com>
2025-11-24 13:21:34 +0200

    final revision

---
+++ Final_Revision/dice_combination.cpp
@@ -0,0 +1,23 @@
+// Idea: Use Dynamic programming (tabulation method and build the table)
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int mod = 1e9 + 7;
+    vector<int> dp(n+1, 0);
+    dp[0] = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <=6; j++){
+            if (i - j >= 0)
+            {
+                dp[i] = (dp[i] + dp[i-j])%mod;
+            }
+        }
+    }
+    cout << dp[n];
+    return 0;
+}
